83de4c95bd38fdf1ff26d3c2ee94598628623e59
Lauri Rooden <lauri@rooden.ee>
2014-01-03 23:11:58 +0200

    Add tests

---
+++ tests/run.js
@@ -0,0 +1,36 @@
+
+var uri = require("../").expand
+
+var test = require("testman")
+.describe("URI Template");
+
+function includeTests(file) {
+	var json = require(process.env.PWD + "/tests/" + file)
+
+	for(var level in json) {
+		var arr = json[level].testcases, len = arr.length, i = 0
+		, args = json[level].variables
+
+		test = test.it("should pass "+level)
+		for (;i<len;i++) {
+			var res = uri(arr[i][0], args)
+			if (Array.isArray(arr[i][1])) {
+				test = test.ok(function(){
+					return arr[i][1].indexOf(res) != -1
+				})
+			} else {
+				test = test.equal(res, arr[i][1])
+			}
+		}
+	}
+}
+
+includeTests("uritemplate-test/spec-examples.json")
+//includeTests("uritemplate-test/spec-examples-by-section.json")
+includeTests("uritemplate-test/extended-tests.json")
+//includeTests("uritemplate-test/negative-tests.json")
+
+
+
+test.done()
+
