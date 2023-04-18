8c197a5c570d42d89b8485080dbde1e9c3178d89
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-04-18 10:58:50 +0200

    change to commonjs bundle
    fixes: #7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+"use strict";
 
 /**
  * @version    20.5.0
@@ -7,133 +7,129 @@
  * @license    MIT License
  */
 
+const URI = {};
 
+/**
+ * URI Template
+ * @see http://tools.ietf.org/html/rfc6570
+ */
 
-!function(URI) {
-	"use strict";
+var RESERVED = /[!'()]/g
+// /[[\]:\/!#$&()*+,;='?@]/g
+, SEPARATORS = {"": ",", "+": ",", "#": ",", "?": "&"}
+, escapeRe = /[$-/?[-^{|}]/g
+, expandRe = /\{([#&+./;?]?)((?:[-\w%.]+(\*|:\d+)?(?:,|(?=})))+)\}/g
+, parseRe  = RegExp(expandRe.source + "|.[^{]*?", "g")
 
-	/**
-	 * URI Template
-	 * @see http://tools.ietf.org/html/rfc6570
-	 */
+URI.encoder = encodeURIComponent
+URI.decoder = decodeURIComponent
 
-	var RESERVED = /[!'()]/g
-	// /[[\]:\/!#$&()*+,;='?@]/g
-	, SEPARATORS = {"": ",", "+": ",", "#": ",", "?": "&"}
-	, escapeRe = /[$-/?[-^{|}]/g
-	, expandRe = /\{([#&+./;?]?)((?:[-\w%.]+(\*|:\d+)?(?:,|(?=})))+)\}/g
-	, parseRe  = RegExp(expandRe.source + "|.[^{]*?", "g")
+/*** EXPAND ***/
+function encodeNormal(val) {
+	return URI.encoder(val).replace(RESERVED, escape)
+}
 
-	URI.encoder = encodeURIComponent
-	URI.decoder = decodeURIComponent
+function notNull(s) {
+	return s != null
+}
 
-	/*** EXPAND ***/
-	function encodeNormal(val) {
-		return URI.encoder(val).replace(RESERVED, escape)
-	}
+function mapCleanJoin(arr, mapFn, joinStr) {
+	arr = arr.map(mapFn).filter(notNull)
+	return arr.length && arr.join(joinStr)
+}
 
-	function notNull(s) {
-		return s != null
-	}
+function expand(template, data) {
+	return template.replace(expandRe, function(_, op, vals) {
+		var sep = SEPARATORS[op] || op
+		, named = sep == ";" || sep == "&"
+		, enc = op && sep == "," ? encodeURI : encodeNormal
+		, out = mapCleanJoin(vals.split(","), function(_name) {
+			var mod = _name.split(/[*:]/)
+			, name = mod[0]
+			, val = data[name]
 
-	function mapCleanJoin(arr, mapFn, joinStr) {
-		arr = arr.map(mapFn).filter(notNull)
-		return arr.length && arr.join(joinStr)
-	}
+			if (val == null) return
 
-	function expand(template, data) {
-		return template.replace(expandRe, function(_, op, vals) {
-			var sep = SEPARATORS[op] || op
-			, named = sep == ";" || sep == "&"
-			, enc = op && sep == "," ? encodeURI : encodeNormal
-			, out = mapCleanJoin(vals.split(","), function(_name) {
-				var mod = _name.split(/[*:]/)
-				, name = mod[0]
-				, val = data[name]
-
-				if (val == null) return
-
-				if (typeof val == "object") {
-					mod = name != _name
-					if (Array.isArray(val)) {
-						val = mapCleanJoin(val, enc, mod ? named ? sep + name + "=" : sep : "," )
-					} else {
-						val = mapCleanJoin(Object.keys(val), function(key) {
-							return enc(key) + (mod ? "=" : ",") + enc(val[key])
-						}, mod && (named || sep == "/") ? sep : ",")
-						if (mod) named = 0
-					}
-					if (!val) return
+			if (typeof val == "object") {
+				mod = name != _name
+				if (Array.isArray(val)) {
+					val = mapCleanJoin(val, enc, mod ? named ? sep + name + "=" : sep : "," )
 				} else {
-					val = enc(mod[1] ? val.slice(0, mod[1]) : val)
+					val = mapCleanJoin(Object.keys(val), function(key) {
+						return enc(key) + (mod ? "=" : ",") + enc(val[key])
+					}, mod && (named || sep == "/") ? sep : ",")
+					if (mod) named = 0
 				}
+				if (!val) return
+			} else {
+				val = enc(mod[1] ? val.slice(0, mod[1]) : val)
+			}
 
-				return (
-					named ?
-					name + (val || sep == "&" ? "=" + val : val) :
-					val
-				)
-			}, sep)
+			return (
+				named ?
+				name + (val || sep == "&" ? "=" + val : val) :
+				val
+			)
+		}, sep)
 
-			return out || out === "" ? (op != "+" ? op + out : out) : ""
-		}
-	)}
+		return out || out === "" ? (op != "+" ? op + out : out) : ""
+	}
+)}
 
-	URI.expand = expand
-	/**/
+URI.expand = expand
+/**/
 
-	URI.Template = function Template(template) {
-		var self = this
-		//if (!(self instanceof Template)) return new Template(template)
-		/*** PARSE ***/
-		, pos = 0
-		, lengths = {}
-		, fnStr = ""
-		, reStr = "^" + template.replace(parseRe, function(_, op, vals) {
-			if (!vals) return escapeRegExp(_)
+URI.Template = function Template(template) {
+	var self = this
+	//if (!(self instanceof Template)) return new Template(template)
+	/*** PARSE ***/
+	, pos = 0
+	, lengths = {}
+	, fnStr = ""
+	, reStr = "^" + template.replace(parseRe, function(_, op, vals) {
+		if (!vals) return escapeRegExp(_)
 
-			var sep = SEPARATORS[op] || op
-			, named = sep == ";" || sep == "&"
-			return vals.split(",").map(function(_name, i) {
-				var mod = _name.split(/[*:]/)
-				, name = mod[0]
-				, re = (lengths[name] || "(") + ".*?)"
+		var sep = SEPARATORS[op] || op
+		, named = sep == ";" || sep == "&"
+		return vals.split(",").map(function(_name, i) {
+			var mod = _name.split(/[*:]/)
+			, name = mod[0]
+			, re = (lengths[name] || "(") + ".*?)"
 
-				pos++
-				if (mod[1]) {
-					re = "((?:%..|.){1," + mod[1] + "})"
-					lengths[name] = "(\\" + pos
-				}
-				fnStr += "t=($[" + pos + "]||'').split('" + (mod ? named ? sep + name + "=" : sep : ",") + "').map(d);"
-				fnStr += "o[\"" + name + "\"]=" + (mod[1] === "" ? "t;" : "t.length>1?t:t[0];")
-				re = escapeRegExp(i === 0 ? op === "+" ? "" : op : sep) + (
-					named ?
-					escapeRegExp(name) + "(?:=" + re + ")?" :
-					sep == "&" ?
-					escapeRegExp(name + "=") + re :
-					re
-				)
-				return mod[1] === "" ? "(?:" + re + ")?" : re
-			}).join("")
-		}) + "$"
-		, re = RegExp(reStr)
-		, fn = Function("$,d", "var t,o={};" + fnStr + "return o")
+			pos++
+			if (mod[1]) {
+				re = "((?:%..|.){1," + mod[1] + "})"
+				lengths[name] = "(\\" + pos
+			}
+			fnStr += "t=($[" + pos + "]||'').split('" + (mod ? named ? sep + name + "=" : sep : ",") + "').map(d);"
+			fnStr += "o[\"" + name + "\"]=" + (mod[1] === "" ? "t;" : "t.length>1?t:t[0];")
+			re = escapeRegExp(i === 0 ? op === "+" ? "" : op : sep) + (
+				named ?
+				escapeRegExp(name) + "(?:=" + re + ")?" :
+				sep == "&" ?
+				escapeRegExp(name + "=") + re :
+				re
+			)
+			return mod[1] === "" ? "(?:" + re + ")?" : re
+		}).join("")
+	}) + "$"
+	, re = RegExp(reStr)
+	, fn = Function("$,d", "var t,o={};" + fnStr + "return o")
 
-		self.template = template
-		self.match = function(uri) {
-			var match = re.exec(uri)
-			return match && fn(match, URI.decoder)
-		}
-
-		function escapeRegExp(string) {
-			return string.replace(escapeRe, "\\$&")
-		}
-		/**/
-		/*** EXPAND ***/
-		self.expand = expand.bind(self, template)
-		/**/
+	self.template = template
+	self.match = function(uri) {
+		var match = re.exec(uri)
+		return match && fn(match, URI.decoder)
 	}
 
-// `this` is `exports` in NodeJS and `window` in browser.
-}(this.URI || (this.URI = {})); // jshint ignore:line
+	function escapeRegExp(string) {
+		return string.replace(escapeRe, "\\$&")
+	}
+	/**/
+	/*** EXPAND ***/
+	self.expand = expand.bind(self, template)
+	/**/
+}
+
+exports.URI = URI;
 
